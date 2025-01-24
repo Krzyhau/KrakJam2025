58cdd0cc819408e1511018fc9efc351403fc5913
Krzyhau <krzyht@gmail.com>
2025-01-24 22:15:08 +0100

    imrpove bubble shape manipulator

--- Assets/Scripts/MotherBubbleShapeManipulator.cs
+++ Assets/Scripts/MotherBubbleShapeManipulator.cs
@@ -12,10 +12,8 @@ namespace Monke.KrakJam2025
 
         [Header("Properties")]
         [Min(4), SerializeField] private int levelOfDetail;
-        [SerializeField] private float insideStretchForce;
-        [SerializeField] private float insideStretchDistance;
-        [SerializeField] private float outsideStretchForce;
-        [SerializeField] private float outsideStretchDistance;
+        [SerializeField] private StretchForceParameters insideStretch;
+        [SerializeField] private StretchForceParameters outsideStretch;
 
         [SerializeField] private List<Transform> insideStretchers = new();
         [SerializeField] private List<Transform> outsideStretchers = new();
@@ -83,6 +81,25 @@ namespace Monke.KrakJam2025
 
                 shapeVertices[i] = circlePoint;
             }
+
+            NormalizeDistancesToSize();
+        }
+
+        private void NormalizeDistancesToSize()
+        {
+            float totalDistances = 0.0f;
+            for (int i = 0; i < shapeVertices.Length; i++)
+            {
+                totalDistances += shapeVertices[i].magnitude;
+            }
+
+            float averageDistance = totalDistances / shapeVertices.Length;
+            float sizeDifference = targetSize / averageDistance;
+
+            for (int i = 0; i < shapeVertices.Length; i++)
+            {
+                shapeVertices[i] *= sizeDifference;
+            }
         }
 
         private float GetStretchForceForPoint(Vector3 vertexPosition)
@@ -92,25 +109,32 @@ namespace Monke.KrakJam2025
 
             foreach (var insideStretcher in insideStretchers)
             {
-                stretchForce += GetStretcherForceInPoint(vertexWorldPoint, insideStretcher, 
-                    insideStretchDistance, insideStretchForce);
+                stretchForce += GetStretcherForceInPoint(vertexWorldPoint, insideStretcher, insideStretch);
             }
 
             foreach (var outsideStretcher in outsideStretchers)
             {
-                stretchForce -= GetStretcherForceInPoint(vertexWorldPoint, outsideStretcher, 
-                    outsideStretchDistance, outsideStretchForce);
+                stretchForce -= GetStretcherForceInPoint(vertexWorldPoint, outsideStretcher, outsideStretch);
             }
 
             return stretchForce;
         }
 
-        float GetStretcherForceInPoint(Vector3 vertexWorldPoint, Transform stretcher, float stretchDistance, float stretchForce)
+        float GetStretcherForceInPoint(Vector3 vertexWorldPoint, Transform stretcher, StretchForceParameters stretchParams)
         {
             Vector3 deltaVector = stretcher.position - vertexWorldPoint;
             Vector3 projectedDeltaVector = Vector3.ProjectOnPlane(deltaVector, transform.forward);
-            float stretchDelta = deltaVector.magnitude / stretchDistance;
-            return Mathf.Lerp(stretchForce, 0f, stretchDelta);
+            float stretchDelta = deltaVector.magnitude / stretchParams.Distance;
+            float stretchExpDelta = Mathf.Pow(stretchDelta, stretchParams.Exponent);
+            return Mathf.Lerp(stretchParams.Force, 0f, stretchExpDelta);
+        }
+
+        [Serializable]
+        public struct StretchForceParameters
+        {
+            public float Distance;
+            public float Exponent;
+            public float Force;
         }
     }
 }
