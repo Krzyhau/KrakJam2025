8c604901b3bbb0bdae3b9d845339bc64a4949b83
MaxPortablePC <dziadon.kakuzu@gmail.com>
2025-01-24 22:04:51 +0100

    working collision force

--- Assets/Scripts/BounceTrigger.cs
+++ Assets/Scripts/BounceTrigger.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 namespace Monke.KrakJam2025
 {
-    public class BounceTrigger : MonoBehaviour
-    {
-        [SerializeField]
-        private Rigidbody2D rb2d;
+	public class BounceTrigger : MonoBehaviour
+	{
+		[SerializeField]
+		private Rigidbody2D rb2d;
 
-        [SerializeField]
-        private float bounceForce;
+		[SerializeField]
+		private float bounceForce;
 
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            var bounceDirection = this.transform.position - collision.gameObject.transform.position;
-            rb2d.AddForce(bounceDirection.normalized * bounceForce, ForceMode2D.Impulse);
-        }
-    }
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			var bounceDirection = this.transform.position - collision.gameObject.transform.position;
+			rb2d.AddForce(bounceDirection.normalized * bounceForce, ForceMode2D.Impulse);
+		}
+	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,32 +3,32 @@ using UnityEngine.InputSystem;
 
 namespace Monke.KrakJam2025
 {
-    public class PlayerController : MonoBehaviour
-    {
-        [SerializeField]
-        private float movementSpeed = 10f;
+	public class PlayerController : MonoBehaviour
+	{
+		[SerializeField]
+		private float movementSpeed = 10f;
 
-        [SerializeField]
-        private Rigidbody2D rb2d;
+		[SerializeField]
+		private Rigidbody2D rb2d;
 
-        private Vector2 cachedInput;
+		private Vector2 cachedInput;
 
-        private void OnMove(InputValue value)
-        {
-            cachedInput = value.Get<Vector2>();
-        }
+		private void OnMove(InputValue value)
+		{
+			cachedInput = value.Get<Vector2>();
+		}
 
-        private void FixedUpdate()
-        {
-            if (rb2d != null)
-            {
-                rb2d.MovePosition(rb2d.position + (movementSpeed * Time.fixedDeltaTime * cachedInput));
-            }
-        }
+		private void FixedUpdate()
+		{
+			if (rb2d != null)
+			{
+				rb2d.AddForce(movementSpeed * Time.fixedDeltaTime * cachedInput, ForceMode2D.Impulse);
+			}
+		}
 
-        private void OnInteract()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+		private void OnInteract()
+		{
+			
+		}
+	}
 }
