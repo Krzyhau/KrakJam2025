089b68bccbbb83988881040b54d914c6bdcf459e
TheMatiaz0 <TheMatiaz0@protonmail.com>
2025-01-25 07:21:16 +0100

    Clean up

--- Assets/Scripts/Mother/MotherPlayerController.cs
+++ Assets/Scripts/Mother/MotherPlayerController.cs
@@ -14,10 +14,16 @@ namespace Monke.KrakJam2025
         private Rigidbody2D rb2d;
 
         [SerializeField]
+        private Transform hamster;
+
+        [SerializeField]
         private float movementMultiplier;
 
         [SerializeField]
         private MotherBubbleContext motherBubbleContext;
+
+        [SerializeField]
+        private MotherBubbleShapeManipulator shapeManipulator;
 
         private List<PlayerBubbleContext> playersInside = new();
 
@@ -48,6 +54,8 @@ namespace Monke.KrakJam2025
             playerContext.Transform.localPosition = Vector2.zero;
 
             OnPlayerAbsorbed?.Invoke(playerContext);
+
+            shapeManipulator.SetStretcherState(hamster, true);
         } 
 
         private void OnPlayerSplit(PlayerBubbleContext playerContext)
@@ -65,6 +73,7 @@ namespace Monke.KrakJam2025
             playerContext.Transform.SetParent(null);
 
             OnPlayerSplitted?.Invoke(playerContext);
+            shapeManipulator.SetStretcherState(hamster, false);
         }
 
         private void FixedUpdate()
@@ -75,7 +84,30 @@ namespace Monke.KrakJam2025
                 float totalInputY = playersInside.Sum(x => x.Input.CachedInput.y);
                 Vector2 totalInput = new(totalInputX, totalInputY);
                 rb2d.AddForce(movementMultiplier * Time.fixedDeltaTime * totalInput, ForceMode2D.Impulse);
+
+                Vector3 currentPosition = hamster.position;
+
+                float angle = Mathf.Atan2(currentPosition.y, currentPosition.x);
+                float currentRadius = currentPosition.magnitude;
+
+                currentRadius = Mathf.Min(currentRadius, radius);
+
+                float x = Mathf.Cos(angle) * currentRadius;
+                float y = Mathf.Sin(angle) * currentRadius;
+
+                float moveX = totalInput.x * moveSpeed * Time.deltaTime;
+                float moveY = totalInput.y * moveSpeed * Time.deltaTime; 
+
+                angle += moveX + moveY;
+
+                float newX = Mathf.Cos(angle) * currentRadius;
+                float newY = Mathf.Sin(angle) * currentRadius;
+
+                hamster.position = new Vector3(newX, newY, currentPosition.z);
             }
         }
+
+        public float radius = 5f;
+        public float moveSpeed = 5f;
     }
 }
