104936117410266bd2c7d989a78ee082725d4ca8
TheMatiaz0 <TheMatiaz0@protonmail.com>
2025-01-25 05:48:24 +0100

    Add two plugins: Honey and Rubin

--- Assets/Scripts/AbsorbTrigger.cs
+++ Assets/Scripts/AbsorbTrigger.cs
@@ -1,3 +1,4 @@
+using Rubin;
 using UnityEngine;
 
 namespace Monke.KrakJam2025
@@ -7,11 +8,18 @@ namespace Monke.KrakJam2025
         [SerializeField]
         private MotherPlayerController mother;
 
+        [SerializeField]
+        private float cooldown = 2;
+
+        private Ticker ticker;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.gameObject.TryGetComponent(out PlayerTrigger playerTrigger) && playerTrigger.PlayerBubbleContext != null)
+            if (ticker.Done && collision.gameObject.TryGetComponent(out PlayerTrigger playerTrigger) && playerTrigger.PlayerBubbleContext != null)
             {
                 mother.AddPlayerInside(playerTrigger.PlayerBubbleContext);
+                ticker = TickerCreator.CreateNormalTime(cooldown);
+                ticker.Reset();
             }
         }
     }
