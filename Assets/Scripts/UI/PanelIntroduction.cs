c35151876e194c3688db3ea1d4457b238184d6c4
TheMatiaz0 <TheMatiaz0@protonmail.com>
2025-01-26 10:57:12 +0100

    fix fix

--- Assets/Scripts/UI/PanelIntroduction.cs
+++ Assets/Scripts/UI/PanelIntroduction.cs
@@ -40,31 +40,29 @@ namespace Monke.KrakJam2025
         {
             flowSystem = FindAnyObjectByType<FlowSystem>();
 
-            seq = DOTween.Sequence();
-
             hubTab.alpha = 0;
             catTab.alpha = 0;
             logo.color = new(1, 1, 1, 0);
             catHandPivot.eulerAngles = new(-187, -177, -140);
 
-            seq.Insert(0, logo.DOFade(1, fadeInDuration))
+            var playerManager = FindAnyObjectByType<PlayerManagerInputHandler>();
+            playerManager.OnPlayerJoin += OnPlayerJoin;
+
+            seq = DOTween.Sequence().Insert(0, logo.DOFade(1, fadeInDuration))
                 .PrependInterval(prependDuration)
                 .Append(logo.DOFade(0, fadeOutDuration))
                 .Append(catTab.DOFade(1, fadeInDuration))
-                .Append(catHandPivot.DORotate(new(0, 0, 90), fadeOutDuration))
+                .Append(catHandPivot.DORotate(new(0, 0, 140), fadeOutDuration))
                 .SetLink(this.gameObject);
-
-            var playerManager = FindAnyObjectByType<PlayerManagerInputHandler>();
-            playerManager.OnPlayerJoin += OnPlayerJoin;
         }
 
         private void OnPlayerJoin()
         {
-            seq?.Kill(true);
+            seq?.Kill();
             var openSilloueteSequence = DOTween.Sequence()
                 .Insert(0, catTab.DOFade(0, fadeOutDuration / 2))
-                .Insert(0, leftBackground.DOMoveX(-1203, fadeOutDuration * 2.5f))
-                .Insert(0, rightBackground.DOMoveX(1203, fadeOutDuration * 2.5f)).SetLink(this.gameObject);
+                .Insert(0, leftBackground.DOMoveX(-2000, fadeOutDuration * 2.5f))
+                .Insert(0, rightBackground.DOMoveX(2000, fadeOutDuration * 2.5f)).SetLink(this.gameObject);
 
             flowSystem.StartGame();
         }
